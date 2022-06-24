cd7a60c2355e7921037409360ad57cbe3305899d
Olav Valle <olavvalle@gmail.com>
2022-06-24 10:56:59 +0200

    add instructions to scp script

--- op-config-files/scp-configs.sh
+++ op-config-files/scp-configs.sh
@@ -1,8 +1,23 @@
 # Utility to SCP OP config, keys and users files to OP instances on network.
+# Usage: place OP config files in the correct directory structure:
+# op-config-files
+# └─ octoprint
+#    ├── config.yaml
+#    ├── data
+#    │   └── appkeys
+#    │       └── keys.yaml
+#    └── users.yaml
 
-for i in {0..5};
+# define the range of IP addresses to run script on.
+# [192.168.1.$IPSTART - 192.168.1.$IPEND]
+IPSTART=50
+IPEND=55
+
+# Run script from op-config-files directory
+
+for i in {$IPSTART..$IPEND};
 do
-  scp -r ./octoprint/* root@192.168.1.5$i:/mnt/dietpi_userdata/octoprint/.octoprint/
-  ssh root@192.168.1.5$i 'sudo chown -R octoprint:octoprint /mnt/dietpi_userdata/octoprint/.octoprint/*'
-  ssh root@192.168.1.5$i '/boot/dietpi/dietpi-services restart octoprint'
+  scp -r ./octoprint/* root@192.168.1.$i:/mnt/dietpi_userdata/octoprint/.octoprint/
+  ssh root@192.168.1.$i 'sudo chown -R octoprint:octoprint /mnt/dietpi_userdata/octoprint/.octoprint/*'
+  ssh root@192.168.1.$i '/boot/dietpi/dietpi-services restart octoprint'
 done
